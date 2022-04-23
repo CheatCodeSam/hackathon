0a254201ad68c130712690c0ccd82d35c034880a
Carson Weeks <CarsonWeeks@mail.com>
2022-04-23 01:49:02 -0700

    Added models.

--- backend/apps/war/admin.py
+++ backend/apps/war/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    pass
--- backend/apps/war/models.py
+++ backend/apps/war/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Quiz(models.Model):
+    quiz_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.quiz_name
+
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=50)
+    right_answer = models.IntegerField()
+    answer1 = models.CharField(null=True, max_length=50)
+    answer2 = models.CharField(null=True, max_length=50)
+    answer3 = models.CharField(null=True, max_length=50)
+    answer4 = models.CharField(null=True, max_length=50)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
+
+    def __str__(self):
+        return self.question_text
